d53d510ddbaf12eb987d32a3f424f233b936e527
mhtariq366 <mhtariq366@gmail.com>
2025-07-28 16:44:41 +0500

    w01l04 output added

---
+++ week01/lesson_04_output.py
@@ -0,0 +1,9 @@
+# Output
+
+"""
+The output function is pretty straight forward. Just use print() function.
+
+We've been using this in past lectrues. You are familiar with this.
+"""
+
+print("Hello")
