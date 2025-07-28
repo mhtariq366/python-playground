36b1276afdcdacae40c0dd8544dd820f0cfebd17
mhtariq366 <mhtariq366@gmail.com>
2025-07-28 16:42:50 +0500

    w01l03 input

---
+++ week01/lesson_03_input.py
@@ -0,0 +1,26 @@
+# Input
+
+# input like in any other language, is taken for various purposes. Python simplifies this via the command
+
+input() 
+
+"""
+This command allows input. But this input is not useful to us yet. We assign this input function to variable, this way, the input
+value will be stored in that particular variable.
+for example: 
+"""
+# var = input()
+
+"""
+this is better. Not perfect yet. Now will would like to display a message for user to view before input. this done by writing message
+in the parenthesis of the input function, enclosed by quotes.
+for example:
+"""
+var = input("Please enter number: ") # this will display a message before getting input:
+
+print(var) # this will now print the input taken.
+
+"""
+An important thing to note down is that, every input is considered to be a string. No matter if your enter numbers only. Initially,
+its considered a string. Verify this by running the type() function we learned in lesson 01.
+"""
