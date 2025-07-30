d4bc1aa0e16199ce771e33bc63738e0afab5411b
mhtariq366 <mhtariq366@gmail.com>
2025-07-30 18:45:12 +0500

    exercise for basics updated

--- basics/exercise_01.py
+++ basics/exercise_01.py
@@ -1,3 +1,8 @@
-# This is exercise 01. Build a calculator using loops, if else, input and output.
+# These are exercise tasks to do. To further show and enhance your skill.
+# 
+# 
+# 
+# 01. Build a calculator using loops, if else, input and output.
+# 02. 
 
 
