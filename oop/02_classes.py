9f4c40f3ef078d7532431a06d6a32f3c50581560
mhtariq366 <mhtariq366@gmail.com>
2025-08-03 19:05:35 +0500

    oop classes added

---
+++ oop/02_classes.py
@@ -0,0 +1,19 @@
+
+"""
+The class is declared using the class keyword accompanied by the class name.
+
+Each class have an __init__ function which will run, the very instant an object is created.
+
+self keyword is used to refer to the current instance of the class. for example, self.v_type means that current objects v_type.
+"""
+
+class Vehicle:
+    def __init__(self, v_type):
+        self.v_type = v_type
+
+    def vehile_type(self):
+        print(f"The vehile type is {self.v_type}")
+
+
+vehicle = Vehicle("Car")    # create an object and pass the parameter to init function.
+vehicle.vehile_type()
