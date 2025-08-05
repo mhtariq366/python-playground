28620aecac39e8edb5fc6f366e929a26c7e46add
mhtariq366 <mhtariq366@gmail.com>
2025-08-05 21:33:35 +0500

    lin_reg in ds - scipy and scatter

--- ds-ai-ml/linear_reg.py
+++ ds-ai-ml/linear_reg.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 import random
+from scipy import stats
+
+def get_y(x):
+    return m * x + c
 
 x = [random.randint(1,20) for _ in range(10)]
 y = [random.randint(1,200) for _ in range(10)]
 
+m, c, r, p, std_err = stats.linregress(x, y)
+
+lin_model = list(map(get_y, x))
+
 plt.scatter(x,y)
+plt.plot(x, lin_model)
 plt.show()
