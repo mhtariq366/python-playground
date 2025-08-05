5aa3bb459edf4c6ff67ba7d6dc935cce7d41ebbc
Hassan <mhtariq366@gmail.com>
2025-08-05 22:50:10 +0500

    lin_reg basics comments added

--- ds-ai-ml/linear_reg.py
+++ ds-ai-ml/linear_reg.py
@@ -1,21 +1,33 @@
+# basic linear reg example
 import matplotlib.pyplot as plt
 import random
 from scipy import stats
 
+# function get values for y
 def get_y(x):
     return m * x + c
 
+# two random generated lists values for x and y
 x = [random.randint(1,20) for _ in range(10)]
 y = [random.randint(1,200) for _ in range(10)]
 
+# get 5 values from stats
 m, c, r, p, err = stats.linregress(x, y)
+
+# m is slope
+# c is y intercept
+# r is the value of relationship. 0 is no relation while 1 and -1 means correlated dataset
+# p is value for hypothesis test
+# err is standard error for slope
 
 lin_model = list(map(get_y, x))
 
+# plot a graph using matplotlib
 plt.scatter(x,y)
 plt.plot(x, lin_model)
 plt.show()
 
+# print to check values. testing. 
 print(f"m: {m}")
 print(f"c: {c}")
 print(f"r: {r}")
