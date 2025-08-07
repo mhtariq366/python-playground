2ed2e772bc2abb85c8a1f9327b9887f4029654dc
mhtariq366 <mhtariq366@gmail.com>
2025-08-07 17:25:53 +0500

    updated lin_reg file

--- ds-ai-ml/lin_reg.py
+++ ds-ai-ml/lin_reg.py
@@ -3,3 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
+df = pd.read_csv('ds-ai-ml/lin_reg.csv')
+
+lin_reg = linear_model.LinearRegression()
+
+lin_reg.fit(df[['area']], df.price)
+
+# predict price of a specific area
+print(lin_reg.predict([[3000]]))
+
+
