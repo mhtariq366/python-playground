cce8c65ca3b8d83bd8fd7ff694b3e4bfe5123be2
mhtariq366 <mhtariq366@gmail.com>
2025-08-07 17:32:34 +0500

    manual and auto calculation

--- ds-ai-ml/lin_reg.py
+++ ds-ai-ml/lin_reg.py
@@ -10,6 +10,20 @@ lin_reg = linear_model.LinearRegression()
 lin_reg.fit(df[['area']], df.price)
 
 # predict price of a specific area
-print(lin_reg.predict([[3000]]))
+print(f"Automatically Calculated: {lin_reg.predict([[3000]])}")
+
+# since linear regression uses y= mx +c . so lets manually perform these and check if calculations are correct.
+
+# get value of m
+m = lin_reg.coef_
+
+# get value of c
+c = lin_reg.intercept_
+
+x = 3000
+
+y = m * x + c
+
+print(f"Manually Calculated: {y}")
 
 
