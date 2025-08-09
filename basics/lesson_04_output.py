80b88fbae25dd0f75e75b13de9e3a11c84946584
Hassan <mhtariq366@gmail.com>
2025-08-09 23:45:33 +0500

    Update lesson_04_output.py

--- basics/lesson_04_output.py
+++ basics/lesson_04_output.py
@@ -7,3 +7,10 @@ We've been using this in past lectrues. You are familiar with this.
 """
 
 print("Hello")
+
+# including variables in print
+num = 5
+print("Number:", num)
+
+# another way
+print(f"Number: {num}")
