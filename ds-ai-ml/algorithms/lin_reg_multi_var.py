9f28fd60e4680e4480d5ff0b7c06d3f9b13a53dc
mhtariq366 <mhtariq366@gmail.com>
2025-08-10 16:19:26 +0500

    preprocess - fillna

--- ds-ai-ml/algorithms/lin_reg_multi_var.py
+++ ds-ai-ml/algorithms/lin_reg_multi_var.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+import math
 
 df = pd.read_csv('ds-ai-ml/lin_reg_multi_var.csv')
+#print(df)
 
+median_bed = math.floor(df['bed'].median())
+mean_age = math.floor(df['age'].mean())
+
+df.bed = df.bed.fillna(median_bed)
+df.age = df.age.fillna(mean_age)
+
+print(df)
