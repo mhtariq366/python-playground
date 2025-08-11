41be7d42cc2e88eb26c9a0e767b21afe2ee1e3ee
mhtariq366 <mhtariq366@gmail.com>
2025-08-11 17:52:26 +0500

    nb scratch - calculate probs of train_set

--- ds-ai-ml/algorithms/nb_from_scratch_imdb_dataset.py
+++ ds-ai-ml/algorithms/nb_from_scratch_imdb_dataset.py
@@ -1,6 +1,7 @@
 # naive bayes implementation on IMDB dataset from scratch, without using pandas, numpy, or sklearn
 
 from collections import defaultdict
+import re
 
 with open('ds-ai-ml/IMDB Dataset.csv', 'r') as f:
     lines = f.readlines()
@@ -16,20 +17,46 @@ train_pos_count=0
 pos_bog = defaultdict(int)
 neg_bog = defaultdict(int)
 
+vocab = []
+
 for i in range(len(train_set)):
     if 'negative' in train_set[i].split(',')[-1]:
         for w in train_set[i].split()[:1]:
-            neg_bog[w.lower()] += 1
+            alpha_num = re.sub(r'[^a-zA-Z0-9]', '', w)
+            alpha_num = alpha_num.lower()
+            neg_bog[alpha_num] += 1
+            if alpha_num not in vocab:
+                vocab.append(alpha_num)
         train_neg_count += 1
     elif 'positive' in train_set[i].split(',')[-1]:
         for w in train_set[i].split()[:1]:
-            pos_bog[w.lower()] += 1
+            alpha_num = re.sub(r'[^a-zA-Z0-9]', '', w)
+            alpha_num = alpha_num.lower()
+            pos_bog[alpha_num] += 1
+            if alpha_num not in vocab:
+                vocab.append(alpha_num)
         train_pos_count += 1
+
 
 prior_prob_neg = train_neg_count/(train_neg_count+train_pos_count)
 prior_prob_pos = train_pos_count/(train_neg_count+train_pos_count)
 
 # print(prior_prob_neg, '---', prior_prob_pos)
 
+# print(len(vocab))
+# print(len(pos_bog))
+# print(len(neg_bog))
 
-# print(neg_bog['the'])
+
+prob_pos_words = defaultdict(int)
+prob_neg_words = defaultdict(int)
+
+for w in pos_bog:
+    prob_pos_words[w] = (pos_bog[w] + 1 ) / (len(pos_bog) + len(vocab))
+
+for w in neg_bog:
+    prob_neg_words[w] = (neg_bog[w] + 1 ) / (len(neg_bog) + len(vocab))
+
+
+print(prob_neg_words['the'])
+
