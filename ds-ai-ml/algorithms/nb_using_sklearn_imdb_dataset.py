5f0eca3976883dcfbfa0d1d95a928434a18982e9
mhtariq366 <mhtariq366@gmail.com>
2025-08-11 20:04:47 +0500

    nb using sklearn - complete, adjustments needed

--- ds-ai-ml/algorithms/nb_using_sklearn_imdb_dataset.py
+++ ds-ai-ml/algorithms/nb_using_sklearn_imdb_dataset.py
@@ -3,6 +3,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
 
 df = pd.read_csv('ds-ai-ml/IMDB Dataset.csv')
 
@@ -15,6 +17,13 @@ df['label'] = df['sentiment'].apply(lambda x: 1 if 'positive' in x else 0)
 train_x, test_x, train_y, test_y = train_test_split(df.review, df.label, test_size=0.2)
 
 vect = CountVectorizer()
-train_x_count = vect.fit_transform(train_x.values)
+train_x_count = vect.fit_transform(train_x)
 
-print(train_x_count.toarray()[45:50])
+# print(train_x_count.toarray()[45:50])
+
+nb_model_mn = MultinomialNB()
+nb_model_mn.fit(train_x_count, train_y)
+
+test_x_count = vect.transform(test_x)
+
+print(f"Multinomial NB: {nb_model_mn.score(test_x_count, test_y)}")
