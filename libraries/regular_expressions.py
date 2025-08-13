6266244a771c13e2226bbc790666d55291ea18f3
Hassan <mhtariq366@gmail.com>
2025-08-13 22:44:58 +0500

    Update regular_expressions.py

--- libraries/regular_expressions.py
+++ libraries/regular_expressions.py
@@ -1,9 +1,12 @@
+# regular expressions
+
 import re
 
 courses = ["artificial intelligence", "data mining", "database", "data structures"]
 
 data_list= []
 
+# among course lits, find course matching specific patterns
 for course in courses:
     res = re.findall("data", course)
     if res:
