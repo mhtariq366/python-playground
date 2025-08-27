82bffdfdae62d2083702d9abfeb4b05d61c689c1
mhtariq366 <mhtariq366@gmail.com>
2025-08-27 19:02:57 +0500

    oop - 01 updated

--- oop/01_classes.py
+++ oop/01_classes.py
@@ -3,8 +3,9 @@
 The class is declared using the class keyword accompanied by the class name.
 
 Each class have an __init__ function which will run, the very instant an object is created.
+__init__ function is often used to assign values to the variables in class.
 
-self keyword is used to refer to the current instance of the class. for example, self.v_type means that current objects v_type.
+self keyword is used to refer to the current instance of the class. for example, self.v_type means that current objects v_type variable.
 """
 
 class Vehicle:
@@ -16,4 +17,4 @@ class Vehicle:
 
 
 vehicle = Vehicle("Car")    # create an object and pass the parameter to init function.
-vehicle.vehile_type() # calling class function
+vehicle.vehile_type()       # calling class function
